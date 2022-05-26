31650644b829c4898c34dd68aaa4e46dd11882db
Dane Dwarka <danedwarka@hotmail.com>
2022-05-26 17:19:21 -0500

    Move state of allMovies to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import MainPage from './components/MainPage';
+import { getMovies } from "./services/movieService"; 
+import { useEffect, useState } from "react";
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+
+  useEffect(() => {getMovies().then(movies => {setAllMovies(movies)})}, []);
+
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <MainPage allMovies={allMovies} />
     </div>
   );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,7 @@
 import TitleList from "./TitleList";
-import { getMovies } from "../services/movieService"; 
-import { useEffect, useState } from "react";
 
-const MainPage = () => {
+const MainPage = ({allMovies}) => {
   const providerNames = ['Netflix', 'Crave', 'Disney', 'Apple Plus'];
-
-  const [allMovies, setAllMovies] = useState([]);
-
-  useEffect(() => {getMovies().then(movies => {setAllMovies(movies)})}, []);
 
   return (
     <div className="MainPage">  
