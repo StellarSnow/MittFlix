637c7c3ebc5c491fed10894599c15e001597f591
Dane Dwarka <danedwarka@hotmail.com>
2022-05-28 11:16:04 -0500

    Move getMovies call from App.js to MainPage Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
 import Header from './components/Header';
 import MainPage from './components/MainPage';
-import { getMovies } from "./services/movieService"; 
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DetailsPage from './components/DetailsPage';
 import SearchPage from "./components/SearchPage";
 import MyWatchListPage from './components/MyWatchListPage';
 
 function App() {
-  const [allMovies, setAllMovies] = useState([]);
-
-  useEffect(() => {getMovies().then(movies => {setAllMovies(movies)})}, []);
 
   return (
     <div className="App">
       <Router>
         <Header />
         <Routes>
-          <Route exact path="/" element={<MainPage allMovies={allMovies} />} />
+          <Route exact path="/" element={<MainPage />} />
           <Route exact path="/details/:detailsId" element={<DetailsPage />} />
           <Route exact path="/search" element={<SearchPage />} />
           <Route exact path="/my-watch-list" element={<MyWatchListPage />} />
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,14 @@
 import TitleList from "./TitleList";
+import { useEffect, useState } from "react";
+import { getMovies } from "../services/movieService"; 
 
-const MainPage = ({allMovies}) => {
+const MainPage = () => {
   const providerNames = ['Netflix', 'Crave', 'Disney', 'Apple Plus'];
+  const [allMovies, setAllMovies] = useState([]);
+
+  useEffect(() => { getMovies().then((movies) => 
+    setAllMovies(movies))
+  }, [])
 
   return (
     <div className="MainPage">  
