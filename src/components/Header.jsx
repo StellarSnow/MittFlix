5c0e928fb0686deca609a6c729a78123204943eb
Dane Dwarka <danedwarka@hotmail.com>
2022-05-29 06:16:13 -0500

    Revert Watch List Hit Area to what it was before

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,15 +24,13 @@ const Header = () => {
   return (
     <header className="header">
       <a href="/"><img src="https://fontmeme.com/permalink/190707/fd4735271a0d997cbe19a04408c896fc.png" alt="netflix-font" border="0" /></a>
-      <Link className="watchlistlink" to="/my-watch-list">
-        <div id="navigation" className="navigation">
-          <nav>
-            <ul>
-              <li>Watch List</li>
-            </ul>
-          </nav>
-        </div>
-      </Link>
+      <div id="navigation" className="navigation">
+        <nav>
+          <ul>
+            <li><Link to="/my-watch-list">Watch List</Link></li>
+          </ul>
+        </nav>
+      </div>
       <form id="search" className="search" onSubmit={onSubmitHandler}>
         <input type="search" placeholder="Search for a title..." onChange={onSearchChangeHandler} value={searchText} />
         <div className="searchResults"></div>
